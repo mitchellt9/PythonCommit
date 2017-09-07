d7559eead2f80fb90cf14b641780703da6036801
mitchellt9 <mitchell.t9@gmail.com>
2017-09-07 10:52:20 +0530

    added time function to generate random subject line

--- sending_emails.py
+++ sending_emails.py
@@ -2,6 +2,8 @@ __author__ = 'Mitchell'
 
 
 import smtplib
+import random
+import time
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -10,6 +12,9 @@ from_address ="ib.wy.test01@gmail.com"
 password = "rosekate9999"
 to="ib.wy.test02@gmail.com"
 server= smtplib.SMTP('smtp.gmail.com', 587)
+# random_number=str(random.random())
+
+
 
 #message = """From:ib.wy.test01@gmail.com To:ankit.tiwari@webyog.com  Subject: SMTP e-mail test
 #This is a test e-mail message. """
@@ -21,7 +26,7 @@ msg = MIMEMultipart()
 #All this goes into the MIMEMultipart for the msg
 msg['From'] = from_address
 msg['To'] = to
-msg['Subject'] = "Python email -a "
+msg['Subject'] = "Python email -a "+ str(time.time())
 body = "Python test mail"
 
 html = """<h1> THis is a test html email <img src = "https://lh3.googleusercontent.com/Vct_XSpNfhherziE-h0mSSpbrJf2jYM1nvPO2PVkcIECnqqUHCf-2-5j8EObefEmS0o7=w300">
@@ -37,9 +42,12 @@ msg.attach(MIMEText(html,'html'))
 content = str(msg)
 
 def sendmail():
-    server.starttls()
-    server.login(user=from_address,password=password)
-    server.sendmail(from_address,to,msg=content)
-
+    try:
+        server.starttls()
+        server.login(user=from_address,password=password)
+        server.sendmail(from_address,to,msg=content)
+        print("Email sent to:"+ to)
+    except:
+        print("Script failed")
 
 sendmail()
